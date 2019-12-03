9b8437930c36d23520e1abefb8639e7140c336b8
Louis-Félix Tessier <louisfelix@gmail.com>
2019-12-03 13:56:04 -0500

    Pin azure-resources-exporter 0.2.0

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #####
 
 locals {
-  application_version = "0.1.0"
+  application_version = "0.2.0"
   labels = {
     "app.kubernetes.io/name"       = "azure-resources-exporter"
     "app.kubernetes.io/component"  = "exporter"
