184e5af2066f40a88b3dec07828159b8f66b69a5
Vincent Creusot <vincent.creusot@fxinnovation.com>
2019-12-10 11:22:38 -0600

    updated version and fixed deprecated

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #####
 
 locals {
-  application_version = "0.6.0"
+  application_version = "0.7.0"
   labels = {
     "app.kubernetes.io/name"       = "azure-resources-exporter"
     "app.kubernetes.io/component"  = "exporter"
